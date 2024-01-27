c73b3f05b7207d8697f95cbcd3240fa66bec04e0
sakura7304moti <sakura7304moti@gmail.com>
2024-01-28 07:39:37 +0900

    sakura1moti用のスケジューラー

---
+++ scheduler_sakura1moti.py
@@ -0,0 +1,37 @@
+from src import moti,utils,const
+import shutil
+
+try:
+    utils.message("scheduler start")
+    model = moti.Scraper()
+    option = const.option_sc()
+    tag = const.hashtags()
+    """
+    BASE
+    """
+    utils.message('base mode start')
+    date, limit = option.base_option()
+    hashtags = tag.base_hashtags()
+    for p in hashtags:
+        print(f'hashtag -> {p}')
+        utils.message(f'hashtag : {p}')
+        model.base_scraper(p, date, limit)
+    utils.message('base mode end')
+    """
+    HOLO
+    """
+    utils.message('holo mode start')
+    date, limit = option.holo_option()
+    model.holo_scraper(date, limit)
+    utils.message('holo mode end')
+    """
+    USER
+    """
+    utils.message("scheduler end")
+    """
+    file copy
+    """
+    shutil.copyfile(const.sqlite_db(),r'/media/share/server/git/sharemotiApi2/scraper/database/twitter.db')
+except Exception as e:
+    print(e)
+    utils.message(e)
