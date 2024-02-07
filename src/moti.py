760d118ef9de077fff4ea03df16608ae51537717
sakura7304moti <sakura7304moti@gmail.com>
2024-02-07 20:47:47 +0900

    selenium無しでスクレイピング

--- src/moti.py
+++ src/moti.py
@@ -1,19 +1,16 @@
-from . import const,utils,sqlite
+from . import const,utils2,sqlite
 from tqdm import tqdm
 holoList = const.holoList()
 
 class Scraper:
     def base_scraper(self, query: str, date: int = 30, limit: int = 3000):
-        driver = utils.get_driver(False)
-        df = utils.get_tweet(query,limit,date,driver)
+        df = utils2.get_tweet(query,date,'base')
         sqlite.update(df,query,'base')
-        driver.close()
         
     def holo_scraper(self, date: int = 30, limit: int = 3000):
-        driver = utils.get_driver(False)
         for query in tqdm(holoList,desc="holo scraper"):
             hashtag = query.hashtag
             print(f'Fanart : {hashtag}')
-            df = utils.get_tweet(hashtag,limit,date,driver)
+            df = utils2.get_tweet(hashtag,date,'holo')
             sqlite.update(df,hashtag,'holo')
         driver.close()
